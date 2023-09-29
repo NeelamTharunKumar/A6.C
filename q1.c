bf41c9c3e83ad9e8458f5aae2707b3ea76f4e588
Tharun Kumar <135581579+NeelamTharunKumar@users.noreply.github.com>
2023-09-29 22:07:02 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -42,48 +42,8 @@ void display(struct Node* node){
     printf("\n");
 }
 
-void polyadd(struct Node* poly1, struct Node* poly2,struct Node* poly){
-    while (poly1->next && poly2->next) {
-        if (poly1->pow > poly2->pow) {
-            poly->pow = poly1->pow;
-            poly->coeff = poly1->coeff;
-            poly1 = poly1->next;
-        }
-        else if (poly1->pow < poly2->pow) {
-            poly->pow = poly2->pow;
-            poly->coeff = poly2->coeff;
-            poly2 = poly2->next;
-        }
-        else {
-            poly->pow = poly1->pow;
-            poly->coeff = poly1->coeff + poly2->coeff;
-            poly1 = poly1->next;
-            poly2 = poly2->next;
-        }
-  
-        poly->next = (struct Node*)malloc(sizeof(struct Node));
-        poly = poly->next;
-        poly->next = NULL;
-    }
-    while (poly1->next || poly2->next) {
-        if (poly1->next) {
-            poly->pow = poly1->pow;
-            poly->coeff = poly1->coeff;
-            poly1 = poly1->next;
-        }
-        if (poly2->next) {
-            poly->pow = poly2->pow;
-            poly->coeff = poly2->coeff;
-            poly2 = poly2->next;
-        }
-        poly->next = (struct Node*)malloc(sizeof(struct Node));
-        poly = poly->next;
-        poly->next = NULL;
-    }
-}
-
 int main(){
-	struct Node *poly1 = NULL, *poly2 = NULL, *poly = NULL;
+	struct Node *poly1 = NULL, *poly2 = NULL;
     create(10, 5, &poly1);
     create(9, 5, &poly1);
     create(3, 2, &poly1);
@@ -93,10 +53,5 @@ int main(){
     
     display(poly1);
     display(poly2);
-    
-    poly = (struct Node*)malloc(sizeof(struct Node));
-  
-    polyadd(poly1, poly2, poly);
-    display(poly);
     return 0;
 }
